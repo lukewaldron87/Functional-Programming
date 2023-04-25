9d0b6b39b63f4d4c3719aa00812239c46ff1f23f
Luke Waldron <lukewaldron87@gmail.com>
2023-04-25 14:25:49 +0200

    error handeling with Optional

--- src/main/functional/FileReaderFromVideo.java
+++ src/main/functional/FileReaderFromVideo.java
@@ -1,12 +1,14 @@
 package main.functional;
 
+import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Comparator;
-import java.util.Map;
+import java.util.*;
 import java.util.function.Function;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class FileReaderFromVideo {
 
@@ -16,12 +18,32 @@ public class FileReaderFromVideo {
     private static final Comparator<Map.Entry<String, Long>> REVERSED_VALUE =
             VALUE_ORDER.reversed();
 
+    public static Optional<Stream<String>> lines(Path p){
+        try{
+            return Optional.of(Files.lines(p));
+        }catch (IOException ioe){
+            System.out.println("File read failed: " + ioe.getMessage());
+            return Optional.empty();
+        }
+    }
+
     public static void main(String args[]) throws Throwable{
+
+        List<String> fileNames = Arrays.asList(
+                "PrideAndPrejudice.txt",
+                "BadBook.txt",
+                "Emma.txt",
+                "SenseAndSensibility.txt"
+        );
 
         String fileName = "PrideAndPrejudice.txt";
 
-        // read file to stream
-        Files.lines(Paths.get(fileName))// get all the lines form the document
+        fileNames.stream()
+                .map(Paths::get)// get a file from the list
+                .map(FileReaderFromVideo::lines)// read the lines of the file
+                .peek(o -> {if (!o.isPresent()) System.err.println("Bad File!");})// check if the optional is empty
+                .filter(Optional::isPresent)//discard empty optionals
+                .flatMap(Optional::get)//extract the stream of string for the valid optionals
                 //.flatMap(l -> WORD_BREAK.splitAsStream(l)) // get the words from each line
                 .flatMap(WORD_BREAK::splitAsStream) // get the words from each line
                 .filter(w -> w.length() > 0)
